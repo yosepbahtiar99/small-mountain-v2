1cecdd48469b4ab1aa431d33f09e7f6004ef1eb9
yosepbahtiar99 <yosep123tiar@gmail.com>
2026-05-06 13:16:35 +0700

    fix: optimize

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,4 +8,16 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
